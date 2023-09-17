8d54a0cd287a91300e43ba6c6f375d7fa84424e7
olamicreas <70097198+olamicreas@users.noreply.github.com>
2023-09-18 00:53:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,14 +22,7 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'olamicreas@gmail.com'
-app.config['MAIL_PASSWORD'] = 'svyp opql amtv gsva'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_DEFAULT_SENDER'] = 'olamicreas@gmail.com'
-mail = Mail(app)
+
 
 
 class Phrase(Form):
